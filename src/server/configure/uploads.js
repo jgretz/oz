5fe76136558b451fc57baa8d62d9a02a0f3e6fed
jgretz <jgretz@truefitsolutions.com>
2016-08-10 09:58:08 -0500

    fix uploads to not assume there is a config

--- src/server/configure/uploads.js
+++ src/server/configure/uploads.js
@@ -2,7 +2,10 @@ import Uploads from '../uploads';
 
 export const UPLOADS = 'UPLOADS';
 export default (app, config) => {
-  const uploads = new Uploads(app, config.uploads);
+  if (!config) {
+    return;
+  }
 
+  const uploads = new Uploads(app, config.uploads);
   app.set(UPLOADS, uploads);
 };
