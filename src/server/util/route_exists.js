6be57ea599c4e7e02efe422decffd45a5ab2c640
jgretz <jgretz@truefitsolutions.com>
2016-09-02 23:13:06 -0400

    update route exists to use the regex

--- src/server/util/route_exists.js
+++ src/server/util/route_exists.js
@@ -1,12 +1,5 @@
 import _ from 'lodash';
 
 export default (router, req) => {
-  // if the route exists, just let it go there
-  const routes = _.uniq(
-    router.stack.map((layer) => {
-      return layer.route.path;
-    })
-  );
-
-  return routes.includes(req.url);
+  return _.some(router.stack, r => req.url.match(r.regexp));
 };
